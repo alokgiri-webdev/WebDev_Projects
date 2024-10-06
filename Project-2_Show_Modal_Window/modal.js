a296bbeb1ef4067cce8694cc69378acb8e972eb2
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-10-07 01:04:33 +0530

    Implement escape  key event

--- Project-2_Show_Modal_Window/modal.js
+++ Project-2_Show_Modal_Window/modal.js
@@ -1,9 +1,11 @@
 'use strict';
+//Selecting all important classes
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnShowModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 
+//Creating Function for Opening the Modal
 function openModal() {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -13,6 +15,7 @@ btnShowModal.forEach(showModalbtn => {
   showModalbtn.addEventListener('click', openModal);
 });
 
+//Creating Function for reseting the project manipulating virtual keys
 function reset() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
@@ -20,3 +23,10 @@ function reset() {
 
 btnCloseModal.addEventListener('click', reset);
 overlay.addEventListener('click', reset);
+
+//Reseting the project manipulating actual key(Escape)
+document.addEventListener('keydown', keyEvent => {
+  if (keyEvent.key === 'Escape' && !modal.classList.contains('hidden')) {
+    reset();
+  }
+});
