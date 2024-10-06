cd63f5b65f28ecd9c205b74f7293285c185b23ad
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-10-07 00:50:06 +0530

    create the project using virtual key

--- Project-2_Show_Modal_Window/modal.js
+++ Project-2_Show_Modal_Window/modal.js
@@ -1,1 +1,22 @@
-"use strict";
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+btnShowModal.forEach(showModalbtn => {
+  showModalbtn.addEventListener('click', openModal);
+});
+
+function reset() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', reset);
+overlay.addEventListener('click', reset);
