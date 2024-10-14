7b3c69e945ab4b792d1a56fe891403abb7eb828b
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-10-15 00:01:50 +0530

    Get current Position, apply marker

--- Project-6_Mapty_App/mapty.js
+++ Project-6_Mapty_App/mapty.js
@@ -9,3 +9,46 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//Getting Current Position
+navigator.geolocation.getCurrentPosition(
+  position => {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+    //Below code is copypasted from the Leaflet website
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    //This 'on' method is only available on 'map' object provided by Leaflet library. 'on' is not of JS
+    map.on('click', mapEvent => {
+      console.log(mapEvent);
+      const { lat, lng } = mapEvent.latlng;
+      //The below code structure is provided by the leaflet Library
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+    });
+  },
+  () => {
+    alert(`Couldn't get your current location`);
+  }
+); // It takes two callbacks first the successCase & second the failureCase
+
+//Display the map using 3rd party Library(Leaflet)
