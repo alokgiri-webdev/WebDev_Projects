d9d067f095fe344e1659d1301ce93304e0200aa0
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-10-15 00:29:39 +0530

    Implementing switch of forms between running & cycling

--- Project-6_Mapty_App/mapty.js
+++ Project-6_Mapty_App/mapty.js
@@ -10,6 +10,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
 //Getting Current Position
 navigator.geolocation.getCurrentPosition(
   position => {
@@ -18,8 +19,10 @@ navigator.geolocation.getCurrentPosition(
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
     const coords = [latitude, longitude];
+
+    //Display the map using 3rd party Library(Leaflet)
     //Below code is copypasted from the Leaflet website
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
@@ -27,23 +30,11 @@ navigator.geolocation.getCurrentPosition(
     }).addTo(map);
 
     //This 'on' method is only available on 'map' object provided by Leaflet library. 'on' is not of JS
-    map.on('click', mapEvent => {
-      console.log(mapEvent);
-      const { lat, lng } = mapEvent.latlng;
-      //The below code structure is provided by the leaflet Library
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent('Workout')
-        .openPopup();
+    //Handling clicks on Map
+    map.on('click', mapE => {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
     });
   },
   () => {
@@ -51,4 +42,35 @@ navigator.geolocation.getCurrentPosition(
   }
 ); // It takes two callbacks first the successCase & second the failureCase
 
-//Display the map using 3rd party Library(Leaflet)
+//Implementing submit form using by trigerring enter key
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  //Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  const { lat, lng } = mapEvent.latlng;
+  //The below code structure is provided by the leaflet Library
+  //Display marker
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+// Implementing switch of forms between running & cycling
+inputType.addEventListener('change', () => {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
